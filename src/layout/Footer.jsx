de4d47e81ad78cda61080943b070d24940cb735d
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-09-13 22:33:33 +0800

    Footer alignment fixed

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -38,7 +38,6 @@ export default function Footer() {
             </ul>
           </div>
 
-
           <div>
             <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
