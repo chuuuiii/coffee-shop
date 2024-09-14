e34de5949d9af94f9f7cc68c315811cdddba54e4
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-09-14 11:38:16 +0800

    Sidebar menu added

---
+++ src/sidebar-menu/SidebarMenu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SidebarMenu() {
+  return (
+    <div className='px-10 mt-5'>
+      <div>
+        Drink
+      </div>
+      <div>
+        Drink
+      </div>
+      <div>
+        Drink
+      </div>
+    </div>
+  )
+}
