0c81ef5a5c40f578c3682532de93e0d6b1b68766
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-09-16 12:39:52 +0800

    image added

--- src/sidebar-menu/SidebarMenu.jsx
+++ src/sidebar-menu/SidebarMenu.jsx
@@ -3,17 +3,17 @@ import { useState } from "react"
 const menuItems = [
   {
     src: "https://cdn.pixabay.com/photo/2023/09/16/15/38/drink-8257030_640.jpg",
-    label: 'juice',
+    label: 'Juice',
     category: 'Drinks'
   },
   {
-    src: "https://cdn.pixabay.com/photo/2016/10/14/18/21/tee-1740871_640.jpg",
+    src: "https://cdn.pixabay.com/photo/2016/11/22/23/48/beverage-1851261_640.jpg",
     label: "Tea",
     category: "Drinks"
   },
   {
-    src: "https://cdn.pixabay.com/photo/2013/08/11/19/46/coffee-171653_1280.jpg",
-    label: "Coffee",
+    src: "https://cdn.pixabay.com/photo/2015/05/31/10/54/coffee-791045_640.jpg",
+    label: "Hot Coffee",
     category: "Drinks"
   }
 ]
@@ -28,7 +28,7 @@ export default function SidebarMenu() {
   return (
     <div className='px-10 mt-5'>
       <div className='text-lg font-semibold flex justify-center space-x-5' onClick={toggleSubItems} style={{ cursor: 'pointer' }}>
-        Drinks
+        <h1>Drinks</h1>
       </div>
       {showSubItems && (
         <div className="mt-5 flex justify-center space-x-5">
